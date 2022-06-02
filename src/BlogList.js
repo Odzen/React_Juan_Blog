7b3cfaf95505775b8166eed44ee511fc11d2d1d6
odzen <jsebastian.va@gmail.com>
2022-06-02 12:59:04 -0500

    Hook useEffect - first steps

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,7 +1,6 @@
 
 const BlogList = (props) => {
-    const blogs = props.blogs
-    const title = props.title
+    const {blogs, title, handleDelete} = props
 
     return(
         <div className="blog-preview">
@@ -10,6 +9,7 @@ const BlogList = (props) => {
                 <div className="blog-preview" key={blog.id}>
                     <h2>{blog.title}</h2>
                     <p>Written by {blog.author}</p>
+                    <button onClick={()=> handleDelete(blog.id)}> Delete Blog</button>
                 </div>
             ))}
         </div>
--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,42 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import BlogList from "./BlogList";
+
+//HOOKS
+// - useState() = Re-renders the server every time the state changes
+// - useEffect() = Call a function from the server every time that happens a re-rendering
+//   ir runs when the state changes and when the component starts
+//   it could be used to fetch data or run an auth service
 
 const Home = () => {
     const [blogs, setBlogs] = useState([
         {title: 'My new website', body : 'lorem ipsum...', author:'Juan', id:1},
         {title: 'Welcome Party', body : 'lorem ipsum...', author:'Sebastian', id:2},
-        {title: 'Web dev top tips', body : 'lorem ipsum...', author:'Velasquez', id:3}
+        {title: 'Web dev top tips', body : 'lorem ipsum...', author:'Juan', id:3}
     ]);
+
+    const [name, setName]= useState('mario');
+
+    const handleDelete = id => {
+        const newBlogs = blogs.filter(blog => blog.id !== id); // To look for the blogs that I want to keep
+        setBlogs(newBlogs);
+    };
+
+    // the empty [] dependency array marks that it won't run if the state changes
+    useEffect(() => {
+        console.log('use Effect ran');
+        console.log(name);
+    }, [name]);
+
     return (
         <div className="home">
-            <BlogList blogs={blogs} title="All blogs!"/>
+            <BlogList blogs={blogs} title="All blogs!" handleDelete={handleDelete}/>
+            <button onClick={() => setName('luigi')}> Change name</button>
+            <p>{name}</p>
         </div>
     );
+
+    // If I want to display a blog written by an specified user
+    //<BlogList blogs={blogs.filter((blog) => blog.author==='Juan' )} title="Juan's blogs!"/>
 }
 
 export default Home;
