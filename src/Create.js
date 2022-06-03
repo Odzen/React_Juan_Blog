ec8819d8293b947db33f6c9c8bd443af5a263b19
odzen <jsebastian.va@gmail.com>
2022-06-03 01:02:22 -0500

    Initial template of form

--- src/Create.js
+++ src/Create.js
@@ -3,6 +3,23 @@ const Create = () => {
     return(
         <div className="create">
             <h2>Add a New Blog</h2>
+            <form>
+                <label> Blog Title: </label>
+                <input
+                    type="text"
+                    required
+                />
+                <label> Blog body: </label>
+                <textarea>
+                    required
+                </textarea>
+                <label>Blog author: </label>
+                <select>
+                    <option value="mario">mario</option>
+                    <option value="mario">mario</option>
+                </select>
+                <button>Add Blog</button>
+            </form>
         </div>
     );
 };
